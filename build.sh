6ad2e34a48e7bfd53e398285b781289f853492e4
Frank Lyaruu <frank@dexels.com>
2019-04-22 23:29:04 +0200

    Added tests

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-docker build . --build-arg VERSION=0.1.249 -t dexels/navajo-demo:1
+BASEBUILDNR=$(curl -s "https://circleci.com/api/v1.1/project/github/Dexels/navajo-platform?circle-token=${CIRCLE_TOKEN}&offset=0&filter=successful" | jq '[.[]| select(.workflows.job_name == "package_navajo")][0].build_num') 
+echo $BASEBUILDNR
+docker-compose build --build-arg VERSION=0.1.${BASEBUILDNR}
+#docker build . --build-arg VERSION=0.1.${BASEBUILDNR} -t dexels/navajo.example:local
 
