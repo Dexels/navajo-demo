981c1cdcf6ce20908ff96974d08e25c35fa18d1a
Frank Lyaruu <frank@dexels.com>
2019-05-22 11:31:55 +0200

    fix deployment, rename sql.rr script

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 LATEST_RELEASE=$(curl -s "https://circleci.com/api/v1.1/project/github/Dexels/navajo.example?circle-token=${CIRCLE_TOKEN}&offset=0&filter=successful" | jq '.[].build_num')
 IMAGE=dexels/navajo.example
 MINORVERSION=0.1
-docker run -p 8181:8181 -it --rm -e FILE_REPOSITORY_DEPLOYMENT=test \
+docker run -p 8181:8181 -it --rm -e FILE_REPOSITORY_DEPLOYMENT=develop \
 	-e INTERACTIVE=true \
 	-e NO_REDIS=true \
 	--name navajo-example \
