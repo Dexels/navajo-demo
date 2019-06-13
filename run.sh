b19818ff738fe07a022256b5986477995c4dc84c
Frank Lyaruu <frank@dexels.com>
2019-06-13 12:58:16 +0200

    added accessstore script to see if actual accessstore elements are appearing

--- run.sh
+++ run.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-LATEST_RELEASE=$(curl -s "https://circleci.com/api/v1.1/project/github/Dexels/navajo.example?circle-token=${CIRCLE_TOKEN}&offset=0&filter=successful" | jq '.[].build_num')
-IMAGE=dexels/navajo.example
-MINORVERSION=0.1
-docker run -p 8181:8181 -it --rm -e FILE_REPOSITORY_DEPLOYMENT=develop \
-	-e INTERACTIVE=true \
-	-e NO_REDIS=true \
-	--name navajo-example \
-	--name navajo-container-example_default \
-	dexels/navajo.example:local
+docker-compose up --build
 	
 
