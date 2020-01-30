4665438af4e657428f358a03a94742caf8ed16fb
Gerco Ballintijn <gballintijn@dexels.com>
2020-01-30 12:01:12 +0100

    Made the run.sh script more versatile.

--- run.sh
+++ run.sh
@@ -5,11 +5,79 @@
 # VERSION refers to latest dexels/navajo-container version.
 #
 
-export PREFIX=3.3
+script=$(basename $0)
 
-export BUILD=$(curl -s "https://circleci.com/api/v1.1/project/github/Dexels/navajo-container?circle-token=${CIRCLE_TOKEN}&limit=1&offset=0&filter=successful" | jq '.[0].build_num')
-export VERSION="${PREFIX}.${BUILD}"
 
-echo "Version: '${VERSION}'"
+function usage() {
 
-docker-compose up --build
+    if [ -n "$1" ] ; then
+        exec >&2
+        echo "$script: Unknown option '$1'"
+    fi
+
+    echo "Usage: $script [-h] [-d] [-k] [-n]"
+    echo "  -h  this info"
+    echo "  -d  dry run"
+    echo "  -k  with the kafka container"
+    echo "  -n  without the navajo container"
+    
+    if [ -n "$1" ] ; then
+        exit 0
+    else
+        exit 1
+    fi
+}
+
+
+function parse_options() {
+    
+    with_kafka=""
+    
+    while getopts :hdkn option ; do
+        case $option in
+            \?)  usage $OPTARG ;;
+            h)  usage ;;
+            d)  dry_run="set" ;;
+            k)  with_kafka="set" ;;
+            n)  with_navajo="" ;;
+        esac
+    done
+        
+    if [ -n "$with_kafka" ] ; then
+        services="$services kafka"
+    fi
+    
+    if [ -n "$with_navajo" ] ; then
+        services="$services navajo"
+        build_option="--build"
+    fi
+}
+
+
+dry_run=""    
+with_navajo="set"
+build_option=""
+services="postgres mongodemo"
+
+parse_options "$@"
+
+
+export VERSION=""
+if [ -n "$with_navajo" ] ; then
+    prefix=3.3
+    if [ -z "$CIRCLE_TOKEN" ] ; then
+        echo "$script: environment variable CIRCLE_TOKEN empty or undefined." >&2
+        exit 2
+    fi
+
+    build=$(curl -s "https://circleci.com/api/v1.1/project/github/Dexels/navajo-container?circle-token=${CIRCLE_TOKEN}&limit=1&offset=0&filter=successful" | jq '.[0].build_num')
+    export VERSION="$prefix.$build"
+    echo "Navajo container version: '${VERSION}'"
+fi
+
+if [ -n "$dry_run" ] ; then
+    echo "dry-run:" docker-compose  up  $build_option  $services
+else
+    echo "services: $services"
+    docker-compose  up  $build_option  $services
+fi
